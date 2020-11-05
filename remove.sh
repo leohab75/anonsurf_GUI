d34b8d359180156b67816453da97127afdcbe8a1
leohab75 <leohab@ya.ru>
2020-11-05 18:34:49 +0300

    main

--- remove.sh
+++ remove.sh
@@ -4,10 +4,8 @@ del=$(zenity --question --text="Удалить 'Anonsurf' с ПК ?? "\
        --width=300 --height=100)
 
 
-if [[ $del == 0 ]]; then
+if [[ $? == 0 ]]; then
        pkexec sudo dpkg -P anonsurf
-
-       rm -rf /opt/anonsurf
-       pkexec rm -f /usr/share/applictionas/anonsurf.desktop
+       pkexec rm -rf /opt/anonsurf
 
 fi
