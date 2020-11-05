1b224b50f9e838888ce061e6ad77d1f47c57f48c
leohab75 <leohab@ya.ru>
2020-11-05 06:54:41 +0300

    testALL commit

--- anon_Gui.sh
+++ anon_Gui.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-sudo apt install zenity -y
+pkexec sudo apt install zenity -y
 
 git clone https://github.com/Und3rf10w/kali-anonsurf.git
 
 cd kali-anonsurf
 
-chmod +x installer.sh
+pkexec sudo chmod +x installer.sh
 
-./installer.sh
+pkexec ./installer.sh
 
-dpkg -i kali-anonsurf.deb
-
-sudo -u $USER mkdir /opt/anon
+mkdir ~/anonsurf
 
 cd ..
 
-sudo -u $USER cp anon.sh /opt/anon
+cp anon.sh ~/anonsurf
 
-chmod +x /opt/anon/anon.sh
+pkexec sudo chmod +x ~/anonsurf/anon.sh
 
-sudo -u $USER cp anonsurf.desktop ~/.local/share/applications/
+cp anonsurf.desktop ~/.local/share/applications/
 
-sudo -u $USER cp anon.png /opt/anon
+cp anon.png ~/anonsurf
 
 
 zenity --question --text="Установить ярлык на рабочий стол ?"\
        --width=300 --height=100
 
     if [[ $? == 0 ]]; then
-        sudo -u $USER cp ~/.local/share/applications/anonsurf.desktop  ~/'Рабочий стол'
+       cp ~/.local/share/applications/anonsurf.desktop  ~/'Рабочий стол'
     fi
 
 
