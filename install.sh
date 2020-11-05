f21da6cfa29d3fae28d6ca2715611ebaabb3dec2
leohab75 <leohab@ya.ru>
2020-11-05 22:28:19 +0300

    main

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 #подготовим почву)
 rm -rf /tmp/anonsurf
-rm -f /home/$USER/'Рабочий стол'/anonsurf.desktop
 
 
 #перемещаем рабочие файлы во временный каталог
@@ -13,7 +12,6 @@ mv -f $(pwd)/remove.sh /tmp/anonsurf
 mv -f $(pwd)/Uninstall-Anonsurf.desktop /tmp/anonsurf
 mv -f $(pwd)/anon.png /tmp/anonsurf
 mv -f $(pwd)/anon.sh /tmp/anonsurf
-cp    $(pwd)/anonsurf.desktop /home/$USER/'Рабочий стол'
 mv -f $(pwd)/anonsurf.desktop /tmp/anonsurf
 mv -f $(pwd)/delete.sh /tmp/anonsurf
 
@@ -26,10 +24,6 @@ desktop-file-validate /usr/share/applications/anonsurf.desktop
 desktop-file-validate /usr/share/applications/Uninstall-Anonsurf.desktop
 install -vdm755 /usr/share/applications && update-desktop-database
 
-
-sleep 10
-
-echo "Готово"
 
 #подчищаем за собой
 rm -rf /tmp/anonsurf
