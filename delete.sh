8719113fd489dfda2e946d588978bbde7aa108dd
leohab75 <leohab@ya.ru>
2020-11-06 06:10:09 +0300

    main

--- delete.sh
+++ delete.sh
@@ -2,6 +2,5 @@
 
 rm -f /usr/share/applications/anonsurf.desktop
 rm -f /usr/share/applications/Uninstall-Anonsurf.desktop
-unlink /usr/share/applications/anonsurf.desktop
 dpkg -P anonsurf
 rm -rf /opt/anonsurf
