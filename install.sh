b009607af6f82446bab3a55b6f87ff14beb2883e
leohab75 <73970447+leohab75@users.noreply.github.com>
2021-05-15 16:29:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,25 +5,15 @@ export RED='\033[1;91m'
 export RESETCOLOR='\033[1;00m'
 export BLUE='\033[1;94m'
 
-rm -rf /tmp/anonsurf
+if [ -f /tmp/anonsurf ]; then
+rm -rv /tmp/anonsurf
+fi
+
 
 mkdir /tmp/anonsurf
 echo -e "\n$GREEN*$BLUE миграция во временный каталог \n$RESETCOLOR"
 
-cp -v  $(pwd)/source/anonsurf.desktop /tmp/anonsurf/
-cp -v  $(pwd)/source/Uninstall-Anonsurf.desktop /tmp/anonsurf/
-cp -v  $(pwd)/source/torrc.anon /tmp/anonsurf/
-cp -v  $(pwd)/source/onion.pac /tmp/anonsurf/
-cp -v  $(pwd)/source/anonsurf.service /tmp/anonsurf/
-cp -v  $(pwd)/source/Anon /tmp/anonsurf/
-cp -v  $(pwd)/source/UnAnonsurf /tmp/anonsurf/
-cp -v  $(pwd)/source/UnAnDelete /tmp/anonsurf/
-cp -v  $(pwd)/source/anonsurf /tmp/anonsurf/
-cp -v  $(pwd)/source/anon.png /tmp/anonsurf/
-cp -v  $(pwd)/source/tor.service /tmp/anonsurf/
-cp -v  $(pwd)/source/ctor.repo /tmp/anonsurf/
-cp -v  $(pwd)/source/ftor.repo /tmp/anonsurf/
-
+cp -rv  $(pwd)/source/ /tmp/anonsurf/
 
 # Добавляем репозитории и
 # устанавливаем Тор
@@ -41,7 +31,7 @@ update-icon-caches /usr/share/pixmaps/*
 ln -s  /usr/share/applications/anonsurf.desktop   /home/$USER/'Рабочий стол'/Anonsurf
 
 echo -e "\n$GREEN*$BLUE Удаляется временный каталог \n$RESETCOLOR"
-rm -rfv /tmp/anonsurf
+rm -rv /tmp/anonsurf
 
 
 echo -e "\n $GREEN Установка звершена с кодом: $RED $? $RESETCOLOR\n"
